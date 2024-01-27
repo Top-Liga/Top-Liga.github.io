f267919417211273b1dc26343aaff8dcdc3ae0f0
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-01-27 18:02:17 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+import os   # импорт модуля работы с каталогами
+import datetime     # модуль для определния текущей даты
+DateNow = str(datetime.datetime.utcnow())[:19].replace(":", "-")    # текущая дата по UTC, отформатированная под строку для имени файла
+
+with open("build/index.html", 'w') as f:
+    f.write('\
+    <!DOCTYPE html>\
+    <html>\
+    <head>\
+        <meta charset="utf-8" />\
+        <title>TopLiga</title>\
+    </head>\
+    <body>\
+        <div class="navbar">\
+            <a href="/" class="navbar-brand">TopLiga</a>\
+        </div>\
+        <div class="body-content">\
+            best games actual standings\
+            <hr />\
+            <footer>\
+                <p>'+DateNow+'</p>\
+            </footer>\
+        </div>\
+    </body>\
+    </html>\
+    ')
