fc537b3ebf28fb72d602cf1bdf299a0c1182c8f6
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-01-27 18:37:22 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,24 +1,27 @@
-import os   # импорт модуля работы с каталогами
-import datetime     # модуль для определния текущей даты
-DateNow = str(datetime.datetime.utcnow())[:19].replace(":", "-")    # текущая дата по UTC, отформатированная под строку для имени файла
-print(DateNow)
-# os.mkdir("build")
-# with open("build/index.html", 'w') as f:
-#     f.write('\
-#     <!DOCTYPE html>\
-#     <html>\
-#     <head>\
-#         <meta charset="utf-8" />\
-#         <title>TopLiga</title>\
-#     </head>\
-#     <body>\
-#         <div class="body-content">\
-#             best games actual standings\
-#             <hr />\
-#             <footer>\
-#                 <p>'+DateNow+'</p>\
-#             </footer>\
-#         </div>\
-#     </body>\
-#     </html>\
-#     ')
+import traceback    # модуль трассировки для отслеживания ошибок
+try:    
+    import os   # импорт модуля работы с каталогами
+    import datetime     # модуль для определния текущей даты
+    DateNow = str(datetime.datetime.utcnow())[:19].replace(":", "-")    # текущая дата по UTC, отформатированная под строку для имени файла
+    os.mkdir("build")
+    with open("build/index.html", 'w') as f:
+        f.write('\
+        <!DOCTYPE html>\
+        <html>\
+        <head>\
+            <meta charset="utf-8" />\
+            <title>TopLiga</title>\
+        </head>\
+        <body>\
+            <div class="body-content">\
+                best games actual standings\
+                <hr />\
+                <footer>\
+                    <p>'+DateNow+'</p>\
+                </footer>\
+            </div>\
+        </body>\
+        </html>\
+        ')
+except: 
+    traceback.print_exc()
