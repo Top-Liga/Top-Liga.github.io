cd66ffe68321a3f56086a8e224a802f2da020891
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-01-27 18:29:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
 import os   # импорт модуля работы с каталогами
 import datetime     # модуль для определния текущей даты
 DateNow = str(datetime.datetime.utcnow())[:19].replace(":", "-")    # текущая дата по UTC, отформатированная под строку для имени файла
-os.mkdir("build")
-with open("build/index.html", 'w') as f:
-    f.write('\
-    <!DOCTYPE html>\
-    <html>\
-    <head>\
-        <meta charset="utf-8" />\
-        <title>TopLiga</title>\
-    </head>\
-    <body>\
-        <div class="body-content">\
-            best games actual standings\
-            <hr />\
-            <footer>\
-                <p>'+DateNow+'</p>\
-            </footer>\
-        </div>\
-    </body>\
-    </html>\
-    ')
+print(DateNow)
+# os.mkdir("build")
+# with open("build/index.html", 'w') as f:
+#     f.write('\
+#     <!DOCTYPE html>\
+#     <html>\
+#     <head>\
+#         <meta charset="utf-8" />\
+#         <title>TopLiga</title>\
+#     </head>\
+#     <body>\
+#         <div class="body-content">\
+#             best games actual standings\
+#             <hr />\
+#             <footer>\
+#                 <p>'+DateNow+'</p>\
+#             </footer>\
+#         </div>\
+#     </body>\
+#     </html>\
+#     ')
